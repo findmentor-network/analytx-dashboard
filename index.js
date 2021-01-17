75eefcb4d238da6b55ec4c651e56a29c0a5e5771
cagatay.cali <cagatay.cali@trendyol.com>
2021-01-17 15:58:20 +0300

    ✨  Removed vue.js dependency

--- index.js
+++ index.js
@@ -4,16 +4,10 @@ const app = express()
 const cors = require("cors")
 const port = 3000
 const {getHREF} = require('./utils')
-const path = __dirname + '/app/dist/';
 
-app.use(express.static(path));
 app.use(express.json());
 app.use(cors());
 app.options("*", cors());
-
-app.get('/', function (req,res) {
-  res.sendFile(`${path}index.html`);
-});
 
 app.get('/count/*', async (req, res) => {
   const { span, range } = req.query;
@@ -22,7 +16,7 @@ app.get('/count/*', async (req, res) => {
   res.json(result)
 })
 
-app.get("/pathVisited/*", async (req, res) => {
+app.get("/visited/*", async (req, res) => {
   const { range } = req.query;
   const hostname = getHREF(req);
   let result = await mostFrequentlyVisited(
